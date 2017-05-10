ef7775c29858a5b60c597ae7632be36c69514ff2
Azin Asili <azinasili@gmail.com>
2017-05-10 00:24:18 -0400

    began build pool of possible characters

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -16,7 +16,7 @@ export default {
     return {
       sharedStore: store.state,
       password: [],
-      capital: [
+      uppercase: [
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
         'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
       ],
@@ -32,15 +32,16 @@ export default {
         '+', '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"', ',', '<', '.', '>',
         '/', '?',
       ],
-      possibleCharacters: [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
-        'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
-        '8', '9', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
-        '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"', ',',
-        '<', '.', '>', '/', '?',
-      ],
+      possibleCharacters: [],
+      // possibleCharacters: [
+      //   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+      //   'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd',
+      //   'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+      //   't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7',
+      //   '8', '9', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+      //   '_', '=', '+', '[', '{', ']', '}', '\\', '|', ';', ':', '\'', '"', ',',
+      //   '<', '.', '>', '/', '?',
+      // ],
     };
   },
   computed: {
@@ -56,6 +57,19 @@ export default {
 
       return this.password;
     },
+  },
+  updated() {
+  // Unsure of lifecycle method
+    const possibleCharacters = [];
+
+    // This does not work too great
+    if (this.sharedStore.uppercase) possibleCharacters.push(...this.uppercase);
+    if (this.sharedStore.lowercase) possibleCharacters.push(...this.lowercase);
+    if (this.sharedStore.symbols) possibleCharacters.push(...this.lowercase);
+
+    // Need to add numbers
+
+    this.possibleCharacters = possibleCharacters;
   },
 };
 </script>
