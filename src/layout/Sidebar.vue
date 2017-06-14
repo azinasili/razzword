7d294c15feb06636e3ce22310f906d1cd36706aa
Azin Asili <azinasili@gmail.com>
2017-06-14 14:16:26 -0400

    Sidebar is now a drawer for mobile devices

--- src/layout/Sidebar.vue
+++ src/layout/Sidebar.vue
@@ -1,14 +1,20 @@
 <template>
-<aside class="sidebar">
+<aside v-bind:class="{'is-open': this.sharedStore.isDrawerOpen }" class="sidebar">
   <pw-options></pw-options>
 </aside>
 </template>
 
 <script>
+import store from '../store';
 import PWoptions from '../components/PWoptions';
 
 export default {
   name: 'sidebar',
+  data() {
+    return {
+      sharedStore: store.state,
+    };
+  },
   components: {
     'pw-options': PWoptions,
   },
@@ -21,10 +27,14 @@ export default {
   padding: 1.25em;
 
   @media (max-width: ((740px/16px) * 1em)) {
+    background-color: rgb(105, 47, 49);
+    height: 100%;
     left: 0;
+    padding-top: 3.75em; // Height of header
     position: absolute;
     top: 0;
-    transform: translate3d(0, -100%, 0);
+    transform: translate3d(-100%, 0, 0);
+    transition: transform .175s ease-in-out;
     width: 100%;
   }
 
@@ -32,5 +42,9 @@ export default {
     order: -1;
     width: ((360px/16px) * 1em);
   }
+}
+
+.sidebar.is-open {
+  transform: translate3d(0, 0, 0);
 }
 </style>
