cd185b3dd08830e1f68333338cce871255a7e22f
Azin Asili <azinasili@gmail.com>
2017-07-09 12:37:19 -0400

    moved src

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,97 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
-  </div>
+<div id="app" class="app">
+  <titlebar></titlebar>
+  <main class="app-body">
+    <pw-content></pw-content>
+    <sidebar></sidebar>
+  </main>
+</div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Titlebar from './layout/Titlebar';
+import PWcontent from './layout/Content';
+import Sidebar from './layout/Sidebar';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    Titlebar,
+    'pw-content': PWcontent,
+    Sidebar,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+html { box-sizing: border-box; height: 100%; }
+*, *::before, *::after { box-sizing: inherit; }
 body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: #ff5f6d;
+  background: radial-gradient(circle at 65% 100%, #ffc371, #ff5f6d);
+  color: rgb(119, 61, 66);
+  font-family: 'Source Sans Pro', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
+  height: 100%;
   margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
 }
 
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.app-body {
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+}
+
+.btn {
+  border: .0625em solid transparent;
+  border-radius: .25em;
+  display: inline-block;
+  cursor: pointer;
+  font-size: 1em;
   font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+  line-height: 1.25;
+  padding: .5em 1em;
+  text-align: center;
+  transition: all .175s ease-in-out;
+  user-select: none;
+  vertical-align: middle;
+  white-space: nowrap;
+
+  .material-icons {
+    display: block;
+    font-size: 1em;
+  }
+}
+
+.btn-primary {
+  background-color: #0275d8;
+  border-color: #0275d8;
+  color: #fff;
+}
+
+.btn-clear {
+  background: none;
+  font-size: 1.15em;
+
+  &:hover,
+  &:focus {
+    background: rgba(255, 255, 255, .26);
+  }
+}
+
+.btn--fade {
+  opacity: .65;
+
+  &:hover,
+  &:focus {
+    opacity: 1;
+  }
 }
 </style>
